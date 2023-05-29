898338b02084edf7d5b899b9f651aba804560b8b
leonardlopez <masasss.the.end@gmail.com>
2023-05-29 11:14:09 -0400

    Create instance of axios

--- src/models/searchs.ts
+++ src/models/searchs.ts
@@ -1,28 +1,43 @@
 import axios from "axios";
 
-
 class Searchs {
-    public history: string[] = ['Caracas', 'Bogota', 'Santiago de Chile', 'Madrid', 'Canada'];
-    constructor() {
-        // TODO: read DB is exists
-    }
+  public history: string[] = [
+    "Caracas",
+    "Bogota",
+    "Santiago de Chile",
+    "Madrid",
+    "Canada",
+  ];
+  constructor() {
+    // TODO: read DB is exists
+  }
 
-    public async city( place: string = '') {
-        try {
-            // request http 
-            // console.log("city: ", place);
-    
-            const request = await axios.get('https://reqres.in/api/users?page=2');
-    
-            console.log(request.data);
-            
-            return []; // return match to the places' find
-        } catch (error) {
-            
-            return [];
+  public async city(place: string = "") {
+    try {
+      // request http
+      
+      const instance = axios.create({
+        baseURL: `https://api.mapbox.com`,
+        params: {
+            'proximity': "ip",
+            'language': "es",
+            'autocomplete': "true",
+            'access_token': "pk.eyJ1IjoibGVvbmFyZGxtNTM4IiwiYSI6ImNsaThjbGR3YzFncHYzbnFwbWJxdjNqaHUifQ.qeVvtuDcvELNVEkQeEmVig",
         }
-        
+      })
+
+    //   instance.defaults.headers.common['Authorization'] = "pk.eyJ1IjoibGVvbmFyZGxtNTM4IiwiYSI6ImNsaThjbGR3YzFncHYzbnFwbWJxdjNqaHUifQ.qeVvtuDcvELNVEkQeEmVig";
+
+      const response = await instance.get(`/geocoding/v5/mapbox.places/${place}.json?`);
+
+      console.log("Hola", response.data);
+
+      return []; // return match to the places' find
+    } catch (error) {
+      console.log(error);
+      ;
     }
+  }
 }
 
 export default Searchs;
